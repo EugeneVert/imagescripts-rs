e06714469a1495e74cbdf18c4932f8f5c39094ae
Eugene Vert <eugene.a.vert@gmail.com>
2021-06-06 18:02:04 +0300

    init find grayscale

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 pub mod modules {
     pub mod find {
         pub mod bpp;
+        pub mod grayscale;
     }
     pub mod cmds;
     pub mod utils;
---
+++ src/modules/find/grayscale.rs
@@ -0,0 +1,80 @@
+use std::{error::Error, ffi::OsString, path::Path};
+
+use rayon::iter::{ParallelBridge, ParallelIterator};
+use structopt::StructOpt;
+
+#[path = "../utils.rs"]
+mod utils;
+
+#[rustfmt::skip]
+#[derive(StructOpt, Debug)]
+#[structopt(name = "imagescripts-rs find", about = " ")]
+struct Opt {
+    #[structopt(required = false, default_value = "./*", display_order = 0)]
+    input: Vec<String>,
+    #[structopt(short, required = false, default_value = "./grayscale", display_order = 0)]
+    out_dir: String,
+    #[structopt(short, default_value = "5")]
+    threshold: usize,
+    #[structopt(long, default_value = "0")]
+    nproc: usize,
+}
+
+pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
+    let opt = Opt::from_iter(args);
+
+    let mut images = opt.input.to_owned();
+    utils::ims_init(&mut images, &opt.out_dir, Some(opt.nproc));
+
+    images
+        .iter()
+        .par_bridge()
+        .for_each(|img| process_image(&img, &opt.out_dir, &opt).unwrap());
+
+    Ok(())
+}
+
+fn process_image(img: &str, out_dir: &str, opt: &Opt) -> Result<(), Box<dyn Error>> {
+    let img_image = image::open(&img)?;
+
+    println!("File: {}", img);
+    if !image_is_colorfull(img_image, opt.threshold) {
+        let save_path = format!(
+            "{}/{}",
+            out_dir,
+            Path::new(img).file_name().unwrap().to_str().unwrap()
+        );
+        std::fs::rename(img, save_path)?;
+    }
+
+    Ok(())
+}
+
+fn image_is_colorfull(img: image::DynamicImage, threshold: usize) -> bool {
+    let thumb_size = 32;
+    if img.color().has_color() {
+        let thumb = img.resize(thumb_size, thumb_size, image::imageops::Nearest);
+
+        let mut is_colorfull = false;
+        let res = thumb.into_rgba8().pixels().par_bridge().find_map_any(|pix | {
+            let pix = pix.0;
+            let pix_r = *pix.get(0).unwrap() as i32;
+            let pix_g = *pix.get(1).unwrap() as i32;
+            let pix_b = *pix.get(2).unwrap() as i32;
+            if std::cmp::max(
+                std::cmp::max((pix_r - pix_g).abs(), (pix_r - pix_b).abs()),
+                (pix_g - pix_b).abs(),
+            ) > threshold as i32
+            {
+                return Some(());
+            }
+            None
+        });
+        if res.is_some() {
+            is_colorfull = true;
+        }
+        return is_colorfull;
+    }
+    //else
+    true
+}
