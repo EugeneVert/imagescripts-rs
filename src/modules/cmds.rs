a4ff34a4872aeb2fb560e7d2e91bc90423b465a2
Eugene Vert <eugene.a.vert@gmail.com>
2021-06-26 21:41:18 +0300

    support for butteraugli

--- src/modules/cmds.rs
+++ src/modules/cmds.rs
@@ -1,6 +1,6 @@
 // TODO: piping input file list, dry run;
 
-use std::{error::Error, ffi::OsString, io::Write, path::Path, str::FromStr};
+use std::{env::temp_dir, error::Error, ffi::OsString, io::{BufRead, Write}, path::Path, str::FromStr};
 
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use structopt::StructOpt;
@@ -16,7 +16,7 @@ struct Opt {
     input: Vec<String>,
     #[structopt(short, takes_value = true, default_value = "./out")]
     out_dir: std::path::PathBuf,
-    #[structopt(short)]
+    #[structopt(short, required = true)]
     cmds: Vec<String>,
     #[structopt(short, long, default_value = "10")]
     tolerance: u32,
@@ -24,6 +24,8 @@ struct Opt {
     save_all: bool,
     #[structopt(long = "csv")]
     save_csv: bool,
+    #[structopt(long = "metrics")]
+    do_metrics: bool,
     #[structopt(long, default_value = "0")]
     nproc: usize,
 }
@@ -35,6 +37,12 @@ pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_iter(args);
 
     let csv_path = "./res.csv";
+
+    let mut opt_metrics = ImageMetricsOptions::new();
+    if opt.do_metrics {
+        opt_metrics.do_metrics = true;
+        opt_metrics.check_availability();
+    }
 
     let mut images = opt.input.to_owned();
     utils::ims_init(&mut images, &opt.out_dir, Some(opt.nproc));
@@ -50,23 +58,34 @@ pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
         let mut csv_writer = csv::WriterBuilder::new()
             .delimiter(b'\t')
             .from_writer(csv_file);
+
+        // csv header row
+        let metrics = opt_metrics.list_avaible();
         let mut csv_row = Vec::from(["", ""]);
         for cmd in &opt.cmds {
             csv_row.push(cmd);
+            for metric in &metrics {
+                csv_row.push(metric);
+            }
         }
         csv_writer.write_record(csv_row)?;
         csv_writer.flush()?;
     }
 
     for img in images {
-        process_image(&img, csv_path, &opt)?;
+        process_image(&img, csv_path, &opt, &opt_metrics)?;
     }
 
     Ok(())
 }
 
 /// Generate results from cmds and compare/save/output them
-fn process_image(img: &str, csv_path: &str, opt: &Opt) -> Result<(), Box<dyn Error>> {
+fn process_image(
+    img: &str,
+    csv_path: &str,
+    opt: &Opt,
+    opt_metrics: &ImageMetricsOptions,
+) -> Result<(), Box<dyn Error>> {
     println!("{}", &img);
     let img_filesize = Path::new(img).metadata().unwrap().len() as u32;
     let img_dimensions = image::image_dimensions(&img)?;
@@ -88,7 +107,7 @@ fn process_image(img: &str, csv_path: &str, opt: &Opt) -> Result<(), Box<dyn Err
     let mut res_filesize: u32 = 0;
     let mut res_buff = ImageBuffer::new("");
 
-    // csv
+    // csv | open writer, push orig image filename&size
     let save_csv = opt.save_csv;
     let mut csv_row = Vec::<String>::new();
     let csv_writer = if save_csv {
@@ -124,6 +143,16 @@ fn process_image(img: &str, csv_path: &str, opt: &Opt) -> Result<(), Box<dyn Err
 
         if save_csv {
             csv_row.push(buff_filesize.to_string());
+            if opt_metrics.do_metrics {
+                let buff_img_decoded = buff.image_decode();
+                if opt_metrics.butteraugli {
+                    let m = opt_metrics
+                        .butteraugli_run(img, buff_img_decoded.path().to_str().unwrap());
+                    csv_row.push(m[0].to_owned());
+                    csv_row.push(m[1].split_once(":").unwrap().1.to_owned());
+                    buff_img_decoded.close().unwrap();
+                }
+            }
         }
 
         if opt.save_all {
@@ -174,6 +203,44 @@ fn process_image(img: &str, csv_path: &str, opt: &Opt) -> Result<(), Box<dyn Err
 }
 
 #[derive(Debug, Clone)]
+struct ImageMetricsOptions {
+    do_metrics: bool,
+    butteraugli: bool,
+}
+
+impl ImageMetricsOptions {
+    fn new() -> ImageMetricsOptions {
+        ImageMetricsOptions {
+            do_metrics: false,
+            butteraugli: false,
+        }
+    }
+    fn check_availability(&mut self) {
+        if is_program_in_path("butteraugli") {
+            self.butteraugli = true;
+        }
+    }
+    fn list_avaible(&self) -> Vec<String> {
+        let mut m: Vec<String> = Vec::new();
+        if self.butteraugli {
+            m.push("butteraugli max norm".into());
+            m.push("butteraugli pnorm".into());
+        }
+        m
+    }
+    fn butteraugli_run(&self, img_orig: &str, img_distort: &str) -> Vec<String> {
+        let out = std::process::Command::new("butteraugli")
+            .arg(img_orig)
+            .arg(img_distort)
+            .output()
+            .unwrap()
+            .stdout;
+        // println!("{:?}", String::from_utf8(out.to_owned()));
+        out.lines().map(|l| l.unwrap()).collect()
+    }
+}
+
+#[derive(Debug, Clone)]
 struct ImageBuffer {
     image: BytesIO,
     cmd: String,
@@ -220,6 +287,21 @@ impl ImageBuffer {
         self.ex_time = time_start.elapsed();
     }
 
+    fn image_decode(&self) -> tempfile::NamedTempFile {
+        let mut tf = tempfile::NamedTempFile::new().unwrap();
+        let tf_out = tempfile::Builder::new().suffix(".png").tempfile().unwrap();
+        tf.write_all(&self.image).unwrap();
+        let decoder = match self.ext.as_str() {
+            "jxl" => "djxl",
+            "avif" => "avifdec",
+            _ => todo!()
+        };
+        std::process::Command::new(decoder)
+            .arg(tf.path())
+            .arg(tf_out.path()).output().unwrap();
+        tf_out
+    }
+
     fn gen_from_cmd(&mut self, img_path: &str, cmd: &str, ext: &str, img_from_stdout: bool) {
         let mut cmd_args: Vec<&str> = self.cmd.split_once(":").unwrap().1.split(' ').collect();
         // no arguments -> return None
@@ -263,4 +345,14 @@ fn byte2size(num: u64) -> String {
         num_f /= 1024.0;
     }
     return format!("{:3.1}TiB", num_f);
+}
+
+fn is_program_in_path(program: &str) -> bool {
+    match std::process::Command::new(program)
+        .spawn()
+        .and_then(|mut x| x.kill())
+    {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
