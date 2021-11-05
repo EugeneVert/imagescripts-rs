0de03ca354a7c9998db4868431af3173e3a4728d
Eugene Vert <eugene.a.vert@gmail.com>
2021-11-05 23:51:46 +0300

    gen video: Add thumbnailer

--- src/modules/generate/video.rs
+++ src/modules/generate/video.rs
@@ -26,7 +26,7 @@ struct Opt {
     #[structopt(long = "bg", default_value = "Black")]
     background: String,
 
-    /// ffmpeg arguments (or preset name {n} ["x264", "x265", "apng", "vp9", "aom-av1", "aom-av1-simple"] )
+    /// ffmpeg arguments (or preset name {n} ["x264", "x265", "apng", "vp9", "aom-av1", "aom-av1-simple"] ) {n}
     #[structopt(short, long = "ffmpeg", default_value = "x264")]
     ffmpeg_args: String,
     /// crf / qscale for preset
@@ -41,6 +41,12 @@ struct Opt {
     /// two-pass video encoding
     #[structopt(long)]
     two_pass: Option<bool>,
+    /// generate video thumbnail
+    #[structopt(short = "t", long = "thumb")]
+    create_thumbnail: bool,
+    /// amount of 2x2 thumbnail sheets
+    #[structopt(short = "s", long = "thumb_sheets", default_value = "2")]
+    thumbnail_sheets: usize,
 }
 
 pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
@@ -89,6 +95,73 @@ pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
         .ok_or_else(|| format!("No filestem: {}", images[0].display()))?;
     utils::ffmpeg_run(&ffmpeg_cmd, output_filestem, two_pass, &container);
 
+    if opt.create_thumbnail {
+        let video_filename = format!("{}.{}", &output_filestem, &container);
+        generate_thumbnail(&video_filename, images.len(), opt.thumbnail_sheets)?;
+    }
+
+    Ok(())
+}
+
+fn generate_thumbnail(
+    video_filename: &str,
+    n_frames: usize,
+    thumbnail_sheets: usize,
+) -> Result<(), std::io::Error> {
+    let tmpdir = tempfile::tempdir()?;
+
+    // Select the most representative frames for each `m = (n / (opt.thumbnail_sheets * 4))` frames from the video
+    // using ffmpeg, and save them to the temporary directory
+    std::process::Command::new("ffmpeg")
+        .arg("-i")
+        .arg(&video_filename)
+        .arg("-vf")
+        .arg(format!(
+            "                                                             \
+            scale=254:254,                                                 \
+            drawtext=fontfile=/usr/share/fonts/noto/NotoSans-Regular.ttf: \
+            fontsize=14:start_number=1:text='%{n}':x=(w-tw)/2:y=h-(2*lh): \
+            fontcolor=white:                                              \
+            bordercolor=black:borderw=1,thumbnail=n={}",
+            {
+                let m = (n_frames as f32 / (thumbnail_sheets as f32 * 4.0)).ceil() as usize;
+                match m {
+                    m if m < 2 => 2,
+                    _ => m,
+                }
+            },
+            n = "{n}"
+        ))
+        .arg("-vsync")
+        .arg("0")
+        .arg(format!("{}/capture%002d.png", &tmpdir.path().display()))
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .output()?;
+
+    const MONTAGE_ARGS_2X2: [&str; 6] = [
+        "-geometry",
+        "+1+1", // add 1px border
+        "-tile",
+        "2x2", // 2x2 images grid
+        "-background",
+        "black",
+    ];
+    std::process::Command::new("montage")
+        .args(MONTAGE_ARGS_2X2)
+        .arg(&format!(
+            "{}/capture%02d.png[1-{}]",
+            &tmpdir.path().display(),
+            &tmpdir.path().read_dir().unwrap().count()
+        ))
+        .arg(format!("{}.jxl", &video_filename))
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .output()?;
+    tmpdir.close()?;
+
     Ok(())
 }
 
@@ -111,14 +184,22 @@ fn get_video_dimm_from_images(images: &[PathBuf]) -> Option<(u32, u32)> {
     println!("{:?}x{:?}", &w, &h);
 
     // find and print image paths whose sizes differs from the most frequent ones
-    images_w
+    let be_resized = images_w
         .iter()
         .zip(&images_h)
         // .for_each(|x| println!("{:?}", &x));
         .enumerate()
         .filter(|x| x.1 != (&w, &h))
         .map(|x| &images[x.0])
-        .for_each(|f| println!("Image {} will be resized", f.display()));
+        .map(|f| {
+            println!("Image {} will be resized", f.display());
+            f
+        })
+        .count() != 0;
+    if be_resized {
+        println!("Please press 'Enter'");
+        std::io::stdin().read_line(&mut String::new()).unwrap();
+    }
 
     Some((w, h))
 }
