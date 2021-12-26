59c6670cbac4ebf1d91f11e07985767bfa9a6ea1
Eugene Vert <eugene.a.vert@gmail.com>
2021-12-26 22:50:34 +0300

    cmds: remove metrics, display cmds stats

--- src/modules/cmds.rs
+++ src/modules/cmds.rs
@@ -1,7 +1,8 @@
 use std::{
+    collections::hash_map::HashMap,
     error::Error,
     ffi::{OsStr, OsString},
-    io::{BufRead, Read, Write},
+    io::Write,
     path::{Path, PathBuf},
 };
 
@@ -41,9 +42,6 @@ struct Opt {
     /// path for csv table
     #[structopt(long = "csv_path", default_value = "./res.csv")]
     csv_path: PathBuf,
-    /// calculate the metrics of the encoded image to the original
-    #[structopt(long = "metrics")]
-    do_metrics: bool,
     #[structopt(long, default_value = "0")]
     nproc: usize,
 }
@@ -55,13 +53,6 @@ pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_iter(args);
 
     let csv_path = &opt.csv_path;
-
-    let mut opt_metrics = ImageMetricsOptions::new();
-    if opt.do_metrics && opt_metrics.check_availability() {
-        opt_metrics.do_metrics = true;
-        let metrics = opt_metrics.list_avaible();
-        println!("Metrics: {:?}", &metrics);
-    }
 
     let mut images = opt.input.to_owned();
     utils::ims_init(&mut images, &opt.out_dir, Some(opt.nproc))?;
@@ -87,9 +78,17 @@ pub fn main(args: Vec<OsString>) -> Result<(), Box<dyn Error>> {
         csv_writer.flush()?;
     }
 
+    let mut resultmap: HashMap<String, usize> = HashMap::new();
     for img in images {
-        process_image(&img, csv_path, &opt, &opt_metrics)?;
+        match process_image(&img, csv_path, &opt) {
+            Ok(c) => *resultmap.entry(c).or_default() += 1,
+            Err(e) => println!("Can't process image {}: {}", &img.display(), &e),
+        }
     }
+    println!("stats: \ncount\t cmd");
+    resultmap
+        .iter()
+        .for_each(|(k, v)| println!("{}\t {}", v, k));
 
     Ok(())
 }
@@ -99,8 +98,7 @@ fn process_image(
     img: &Path,
     csv_path: &Path,
     opt: &Opt,
-    opt_metrics: &ImageMetricsOptions,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<String, Box<dyn Error + Send + Sync>> {
     println!("{}", &img.display());
     let img_filesize = img.metadata()?.len() as u32;
     let img_dimensions = image::image_dimensions(&img)?;
@@ -114,16 +112,9 @@ fn process_image(
         .par_iter()
         .map(|cmd| {
             let mut buff = ImageBuffer::new();
-            buff.image_generate(img, cmd).unwrap_or_else(|e| {
-                // TODO match Result<Vec<_>>
-                eprintln!("Can't process {}: {}", &cmd, &e);
-                if !confirm_prompt("Continue?") {
-                    std::process::exit(1);
-                }
-            });
-            buff
+            buff.image_generate(img, cmd).map(|_| buff)
         })
-        .collect();
+        .collect::<Result<_, _>>()?;
 
     let mut res_filesize: u32 = 0;
     let mut res_buff = &ImageBuffer::new();
@@ -153,7 +144,7 @@ fn process_image(
         let buff_filesize = buff.get_image_size() as u32;
         let buff_bpp = (buff_filesize * 8) as f64 / px_count as f64;
         let percentage_of_original = format!("{:.2}", (100 * buff_filesize / img_filesize));
-        let mut printing_status = format!(
+        let printing_status = format!(
             "{}\n{} --> {}\t{:6.2}bpp\t{}%\t{:>6.2}s",
             &buff.get_cmd(),
             byte2size(img_filesize as u64),
@@ -162,35 +153,6 @@ fn process_image(
             percentage_of_original,
             &buff.ex_time.as_secs_f32(),
         );
-
-        if opt_metrics.do_metrics {
-            // remova alpha from source and distorted images
-            // FIXME sometimes doesn't gives ?lossy? results
-            let img_wa = image_remove_alpha(img)?;
-            let img_wa_path = img_wa.path().to_str().unwrap().to_string();
-            let img_distorted = buff.image_decode()?;
-            let img_distorted_wa = image_remove_alpha(img_distorted.path())?;
-            let img_distorted_path = img_distorted_wa.path().to_str().unwrap().to_string();
-            img_distorted.close()?;
-
-            // do metrics
-            if opt_metrics.butteraugli {
-                let m = opt_metrics.butteraugli_run(&img_wa_path, &img_distorted_path)?;
-                let butteraugli_max_norm = &m[0];
-                let butteraugli_pnorm = m[1].split_once(":").unwrap().1;
-                printing_status = printing_status
-                    + "butteraugli_max_norm: "
-                    + butteraugli_max_norm
-                    + "\t butteraugli_pnorm: "
-                    + butteraugli_pnorm
-                    + "\t "
-            }
-            if opt_metrics.ssimulacra {
-                let m = opt_metrics.ssimulacra_run(&img_wa_path, &img_distorted_path)?;
-                let ssimulacra = &m;
-                printing_status = printing_status + "ssimulacra: " + ssimulacra
-            }
-        }
 
         println!("{}", printing_status);
 
@@ -237,7 +199,7 @@ fn process_image(
     }
 
     if opt.save_all {
-        return Ok(());
+        return Ok("".into());
     }
     // save res_buf
     let save_path = out_dir.join(format!(
@@ -251,85 +213,7 @@ fn process_image(
     f.write_all(&res_buff.image)?;
     println!("save {}\n", &res_buff.cmd);
 
-    Ok(())
-}
-
-#[derive(Debug, Clone)]
-struct ImageMetricsOptions {
-    do_metrics: bool,
-    butteraugli: bool,
-    ssimulacra: bool,
-}
-
-impl ImageMetricsOptions {
-    fn new() -> ImageMetricsOptions {
-        ImageMetricsOptions {
-            do_metrics: false,
-            butteraugli: false,
-            ssimulacra: false,
-        }
-    }
-
-    /// Checks the metrics avaibility in path and sets the corresponding struct field to `true`
-    /// # Returns
-    /// `true` if any metric is avaible
-    fn check_availability(&mut self) -> bool {
-        if is_program_in_path("butteraugli_main") {
-            self.butteraugli = true;
-        } else {
-            println!("No butteraugli_main in PATH")
-        }
-        if is_program_in_path("ssimulacra_main") {
-            self.ssimulacra = true;
-        } else {
-            println!("No ssimulacra_main in PATH")
-        }
-        !(self.butteraugli | self.ssimulacra)
-    }
-
-    /// returns a vec of avaible metrics
-    fn list_avaible(&self) -> Vec<String> {
-        let mut m: Vec<String> = Vec::new();
-        if self.butteraugli {
-            m.push("butteraugli max norm".into());
-            m.push("butteraugli pnorm".into());
-        }
-        if self.ssimulacra {
-            m.push("ssimulacra".into());
-        }
-        m
-    }
-
-    fn butteraugli_run(
-        &self,
-        original: &str,
-        distorted: &str,
-    ) -> Result<Vec<String>, Box<dyn Error>> {
-        let outp = std::process::Command::new("butteraugli_main")
-            .arg(original)
-            .arg(distorted)
-            .output()?;
-        command_print_if_error(&outp)?;
-        Ok(outp
-            .stdout
-            .lines()
-            // write '-' to csv if no output
-            .map(|l| l.unwrap_or_else(|_| "-".into()))
-            .collect())
-    }
-
-    fn ssimulacra_run(&self, original: &str, distorted: &str) -> Result<String, Box<dyn Error>> {
-        let outp = std::process::Command::new("ssimulacra_main")
-            .arg(original)
-            .arg(distorted)
-            .output()?;
-        command_print_if_error(&outp)?;
-        Ok(outp
-            .stdout
-            .lines()
-            .next()
-            .unwrap_or_else(|| Result::Ok("-".into()))?)
-    }
+    Ok(res_buff.cmd.to_string())
 }
 
 #[derive(Debug, Clone)]
@@ -368,8 +252,12 @@ impl<'a> ImageBuffer<'a> {
         self.cmd_enc.to_string() + " " + &self.cmd_enc_args.join(" ")
     }
 
-    fn image_generate(&mut self, img_path: &Path, cmd: &'a str) -> Result<(), Box<dyn Error>> {
-        self.cmd = &cmd;
+    fn image_generate(
+        &mut self,
+        img_path: &Path,
+        cmd: &'a str,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        self.cmd = cmd;
         let cmd_args: Vec<String> = cmd.split(">:").map(|s| s.to_owned()).collect();
         let time_start = std::time::Instant::now();
 
@@ -382,7 +270,11 @@ impl<'a> ImageBuffer<'a> {
         Ok(())
     }
 
-    fn image_generate_preset(&mut self, img_path: &Path, cmd: &str) -> Result<(), Box<dyn Error>> {
+    fn image_generate_preset(
+        &mut self,
+        img_path: &Path,
+        cmd: &str,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
         let cmd_preset = cmd.split_once(":").ok_or("Cmd argument error")?.0;
         self.cmd_enc_args = cmd
             .split_once(":")
@@ -430,7 +322,7 @@ impl<'a> ImageBuffer<'a> {
         &mut self,
         img_path: &Path,
         cmd_args: &[String],
-    ) -> Result<(), Box<dyn Error>> {
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
         self.cmd_enc_args = cmd_args[4].split(' ').map(|s| s.to_owned()).collect();
         self.ext = cmd_args[0].to_string();
         self.cmd_enc = cmd_args[1].to_string();
@@ -440,20 +332,20 @@ impl<'a> ImageBuffer<'a> {
         Ok(())
     }
 
-    fn image_decode(&self) -> Result<tempfile::NamedTempFile, Box<dyn Error>> {
-        let mut tf = tempfile::NamedTempFile::new()?;
-        let tf_out = tempfile::Builder::new().suffix(".png").tempfile()?;
-        tf.write_all(&self.image)?;
-        let outp = std::process::Command::new(&self.cmd_dec)
-            .arg(tf.path())
-            .args(&self.cmd_dec_args)
-            .arg(tf_out.path())
-            .output()?;
-        command_print_if_error(&outp)?;
-        Ok(tf_out)
-    }
+    // fn image_decode(&self) -> Result<tempfile::NamedTempFile, Box<dyn Error>> {
+    //     let mut tf = tempfile::NamedTempFile::new()?;
+    //     let tf_out = tempfile::Builder::new().suffix(".png").tempfile()?;
+    //     tf.write_all(&self.image)?;
+    //     let outp = std::process::Command::new(&self.cmd_dec)
+    //         .arg(tf.path())
+    //         .args(&self.cmd_dec_args)
+    //         .arg(tf_out.path())
+    //         .output()?;
+    //     command_print_if_error(&outp)?;
+    //     Ok(tf_out)
+    // }
 
-    fn gen_from_cmd(&mut self, img_path: &Path) -> Result<(), Box<dyn Error>> {
+    fn gen_from_cmd(&mut self, img_path: &Path) -> Result<(), Box<dyn Error + Send + Sync>> {
         // no arguments -> return None
         if self.cmd_enc_args.contains(&"".into()) {
             self.cmd_enc_args.pop();
@@ -483,22 +375,6 @@ impl<'a> ImageBuffer<'a> {
     }
 }
 
-/// remove alpha from image using ImageMagick convert
-fn image_remove_alpha(img: &Path) -> Result<tempfile::NamedTempFile, Box<dyn Error>> {
-    let tf = tempfile::Builder::new().suffix(".png").tempfile()?;
-    // TODO find a way to do alpha-removal w/o ImageMagick
-    // let img = image::open(img)?.to_rgb8();
-    // img.save_with_format(tf.path(), image::ImageFormat::Png)?;
-
-    let outp = std::process::Command::new("convert")
-        .arg(img)
-        .args(["-alpha", "off"])
-        .arg("PNG24:".to_string() + tf.path().to_str().unwrap())
-        .output()?;
-    command_print_if_error(&outp)?;
-    Ok(tf)
-}
-
 fn command_print_if_error(output: &std::process::Output) -> Result<(), String> {
     if output.status.success() {
         Ok(())
@@ -507,19 +383,6 @@ fn command_print_if_error(output: &std::process::Output) -> Result<(), String> {
         println!("{}", std::str::from_utf8(&o).unwrap());
         Err("Command returned error status".into())
     }
-}
-
-// TODO
-fn confirm_prompt(text: &str) -> bool {
-    println!("{} [Y/n] ", text);
-    let mut i = [0];
-    std::io::stdin().lock().read_exact(&mut i).unwrap();
-    println!("{:?}", &i);
-    println!("{:?}", String::from_utf8_lossy(&i));
-    if String::from_utf8_lossy(&i) == "y" {
-        return true;
-    }
-    false
 }
 
 fn byte2size(num: u64) -> String {
@@ -531,11 +394,4 @@ fn byte2size(num: u64) -> String {
         num_f /= 1024.0;
     }
     return format!("{:3.1}TiB", num_f);
-}
-
-fn is_program_in_path(program: &str) -> bool {
-    std::process::Command::new(program)
-        .spawn()
-        .and_then(|mut x| x.kill())
-        .is_ok()
 }
